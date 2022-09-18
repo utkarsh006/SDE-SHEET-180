9af09f0f7d067f72884356f0588954bfec219fda
Utkarsh Saxena <94545831+utkarsh006@users.noreply.github.com>
2022-09-18 20:19:35 +0530

    Create Majority Element.cpp

---
+++ ARRAYS AND MATRIX/Majority Element.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums)
+    {
+       unordered_map<int,int> mp; 
+        int n = nums.size();
+        
+        //creating hashmap
+        for(int i=0; i<n; i++)
+            mp[nums[i]]++;
+        
+        for(auto x: mp) // accessing key value pairs 2->3, 3->1....
+        {
+            if(x.second > n/2)
+                return x.first;
+        }
+        
+        return -1;
+    }
+    
+};
+
+// OR 
+// Moore Voting Algorithm
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums)
+    {
+        int counter = 0, majority;
+        for (int num : nums) 
+        {
+            if (!counter) 
+                majority = num;
+            
+            counter += num == majority ? 1 : -1;
+        }
+        return majority;
+    }
+};
