19d8d684670a7c642e1b9eb883c97e2d8f3230c6
Utkarsh Saxena <94545831+utkarsh006@users.noreply.github.com>
2022-09-25 12:00:33 +0530

    Update Kadane's Algo.cpp

--- ARRAYS AND MATRIX/Kadane's Algo.cpp
+++ ARRAYS AND MATRIX/Kadane's Algo.cpp
@@ -1,4 +1,10 @@
- long long maxSubarraySum(int arr[], int n)
+/* 1. Take out the sum of each element in array.
+   2. Store the currsum in sum and take maxi as INT_MIN.
+   3. If sum>maxi then set maxi=sum. 
+   4. When you encounter negative element set sum=0.
+   5. Return maxi.   */
+
+long long maxSubarraySum(int arr[], int n)
     {
          int sum=0, maxi= INT_MIN;
         
