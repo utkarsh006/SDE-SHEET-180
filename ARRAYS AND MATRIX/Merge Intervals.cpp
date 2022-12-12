55c7404700141c0e68f396daa3df9246e768881c
Utkarsh Saxena <94545831+utkarsh006@users.noreply.github.com>
2022-12-12 23:01:15 +0530

    Create Merge Intervals.cpp

---
+++ ARRAYS AND MATRIX/Merge Intervals.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) 
+    {
+        if(intervals.size()<=1) return intervals;
+       
+        vector<vector<int>>ans;
+        
+        sort(intervals.begin(), intervals.end()); 
+        
+        ans.push_back(intervals[0]);
+         
+           for(int i=1; i<intervals.size(); i++) 
+           {
+             //if intervals are overlapping
+            if(ans.back()[1] >= intervals[i][0])
+             ans.back()[1] = max(ans.back()[1],intervals[i][1]);
+            
+            else 
+              ans.push_back(intervals[i]); 
+        }
+        return ans;
+    }
+};
+
+// TIME COMPLEXITY : O(N logN)
+// SPACE COMPLEXITY : O(N) to return the answer of the merged intervals.
